b2c332485d6752e2b5c531667a13c226ae442cb3
medev2e <medev2e@gmail.com>
2025-04-27 10:51:24 -0500

    Cambios en DatabaseConfig

--- src/main/java/com/biblioteca/atenea/config/DatabaseConfig.java
+++ src/main/java/com/biblioteca/atenea/config/DatabaseConfig.java
@@ -1,12 +1,16 @@
 package com.biblioteca.atenea.config;
 
 import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.nio.file.Files;
 
 public class DatabaseConfig {
     private static final String DB_DIRECTORY = "var/db/";
-    private static final String[] DB_ROUTE = { buildRoute("users.db"), buildRoute("employees.db"),
-            buildRoute("books.db") };
+    private static final String USERS_DB = buildRoute("users.db");
+    private static final String EMPLOYEES_DB = buildRoute("employees.db");
+    private static final String BOOKS_DB = buildRoute("books.db");
 
     private static String buildRoute(String fileName) {
         return DB_DIRECTORY + fileName;
@@ -20,18 +24,23 @@ public class DatabaseConfig {
             if (Files.notExists(dbDirectoryPath)) {
                 Files.createDirectories(dbDirectoryPath);
             }
-
-            for (int i = 0; i < DB_ROUTE.length; i++) {
-
-                Path dbFilesPath = Path.of(DB_ROUTE[i]);
-
-                if (Files.notExists(dbFilesPath)) {
-                    Files.createFile(dbFilesPath);
-                }
-            }
         } catch (Exception e) {
 
         }
     }
 
+    public static Connection getUsersConnection() throws SQLException {
+        checkRouteExist();
+        return DriverManager.getConnection("jdbc:sqlite:" + USERS_DB);
+    }
+
+    public static Connection getEmployeesConnection() throws SQLException {
+        checkRouteExist();
+        return DriverManager.getConnection("jdbc:sqlite:" + EMPLOYEES_DB);
+    }
+
+    public static Connection getBooksConnection() throws SQLException {
+        checkRouteExist();
+        return DriverManager.getConnection("jdbc:sqlite:" + BOOKS_DB);
+    }
 }
